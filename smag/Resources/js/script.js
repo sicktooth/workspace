df02366917fa06054b0c876a97c0c6998a948158
sicktooth <sosaronungwe@gmail.com>
2023-07-25 23:22:51 +0100

    previous commit

--- smag/Resources/js/script.js
+++ smag/Resources/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     
-   $('.mobile__icon').click(function(){
+    
+    $('ion-icon[name=menu-outline]').click(function(){
        var nav = $('.center__nav').addClass('js__mobile__nav');
-       nav.slideToggle(200);
+       nav.slideToggle(250);
    });
 });
