a7aa9bcb309fcffb027aa8c75d21eebda3676380
sicktooth <sosaronungwe@gmail.com>
2023-07-26 23:15:05 +0100

    working on the script

--- smag/Resources/js/script.js
+++ smag/Resources/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-    
-    
+
     $('ion-icon[name=menu-outline]').click(function(){
        var nav = $('.center__nav').addClass('js__mobile__nav');
        nav.slideToggle(250);
    });
+    
+    $('.js__header__box').waypoint(function(direction){
+        if (direction == "down") {
+            $('.js__nav').addClass('home__nav__sticky');
+        }
+        else {
+            $('.js__nav').removeClass('home__nav__sticky');
+        }
+    });
 });
