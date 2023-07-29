54e85da640e32ee520fc0e4e746566fbd42e4f26
sicktooth <sosaronungwe@gmail.com>
2023-07-29 23:40:50 +0100

    done

--- smag/Resources/js/script.js
+++ smag/Resources/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('ion-icon[name=menu-outline]').click(function(){
        var nav = $('.center__nav').addClass('js__mobile__nav');
        nav.slideToggle(250);
-   });
+        });
     
     $('.js__header__box').waypoint(function(direction){
         if (direction == "down") {
@@ -12,5 +12,29 @@ $(document).ready(function(){
         else {
             $('.js__nav').removeClass('home__nav__sticky');
         }
-    });
+            });
+    
+    $('.js__wp__vip').waypoint(function(direction){
+        $('.js__wp__vip').addClass('animate__animated animate__shakeY');
+    }, {offset:'50%;'});
+    
+    $('.js__wp__1').waypoint(function(direction){
+        $('.js__wp__1').addClass('animate__animated animate__rotateIn');
+    }, {offset:'50%;'});
+    
+    $('.js__wp__2').waypoint(function(direction){
+        $('.js__wp__2').addClass('animate__animated animate__rollIn');
+    }, {offset:'50%;'});
+    
+    $('.js__wp__3').waypoint(function(direction){
+        $('.js__wp__3').addClass('animate__animated animate__rollIn');
+    }, {offset:'50%;'});
+    
+    $('.js__wp__footer').waypoint(function(direction){
+        $('.js__wp__footer').addClass('animate__animated animate__heartBeat');
+    }, {offset:'80%;'});
+    
+    $('.js__wp__cities').waypoint(function(direction){
+        $('.js__wp__city').addClass('animate__animated animate__lightSpeedInLeft');
+    }, {offset:'40%;'});
 });
